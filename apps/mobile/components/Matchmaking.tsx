7aac49f335449891bf17902444ebd711a58cb021
nyapblesso-a11y <nyapblesso@gmail.com>
2026-07-23 17:04:03 +0100

    formatted using prettier

--- apps/mobile/components/Matchmaking.tsx
+++ apps/mobile/components/Matchmaking.tsx
@@ -8,7 +8,7 @@ import {
   Image,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons} from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
